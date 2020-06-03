ab0b30510c3953b9ba4fa5640f4033e4c82d9ce6
dmdekf <dmdekf@gmail.com>
2020-06-03 10:22:51 +0900

    Add VideoDetail

--- src/App.vue
+++ src/App.vue
@@ -2,49 +2,68 @@
   <div class="container">
     <!-- emit 2. 듣고, "a" -->
     <SearchBar @input-change="onInputChange" />
-    <VideoList :videos="videos" />
+    <div class="row">
+      <VideoDetail :selectedVideo="selectedVideo" />
+      <VideoList @video-select="onVideoSelect" :videos="videos" />
+      <VideoPlay :videos="videos" />
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
-import SearchBar from './components/SearchBar.vue'
-import VideoList from './components/VideoList.vue'
-
-const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY  // 비활성화된 키
-const API_URL = 'https://www.googleapis.com/youtube/v3/search'
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoPlay from "./components/VideoPlay";
+import VideoDetail from "./components/VideoDetail";
+const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
+const API_URL = "https://www.googleapis.com/youtube/v3/search";
 
 export default {
-  name: 'App',
-  components: { SearchBar, VideoList, },
+  name: "App",
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
   data() {
     return {
-      inputValue: '',
-      videos: [],
-    }
+      inputValue: "",
+      videos: null,
+      selectedVideo: null
+    };
   },
   methods: {
     onInputChange(inputText) {
-      this.inputValue = inputText
-      // console.log(API_KEY)
-
-      axios.get(API_URL, {
-        params: {
-          key: API_KEY,
-          part: 'snippet',
-          type: 'video',
-          q: this.inputValue,
-          maxResults: 10
-        }
-      })
-        .then(res => this.videos = res.data.items)
-        .catch(err => console.error(err))
+      console.log(`input-change이벤트발생 : ${inputText}`);
+      this.inputValue = inputText;
+      axios
+        .get(API_URL, {
+          params: {
+            key: API_KEY,
+            part: "snippet",
+            type: "video",
+            q: this.inputValue,
+            maxResults: 4
+          }
+        })
+        .then(res => {
+          res.data.items.forEach(item => {
+            // html 내부 &#1234 를 처리, v-html 대신.
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(item.snippet.title, "text/html");
+            item.snippet.title = doc.body.innerText;
+          });
+          this.videos = res.data.items;
+        })
+        .catch(err => console.error(err));
     },
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
